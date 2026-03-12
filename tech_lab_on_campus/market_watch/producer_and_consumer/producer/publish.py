277006c3cf81a4df291e257c25f1f58b9c6ed6f3
Karen Mkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2026-03-12 01:28:24 +0000

    did basic publish

--- tech_lab_on_campus/market_watch/producer_and_consumer/producer/publish.py
+++ tech_lab_on_campus/market_watch/producer_and_consumer/producer/publish.py
@@ -22,7 +22,7 @@ from solution.producer_sol import mqProducer  # pylint: disable=import-error
 
 
 def main() -> None:
-    producer = mqProducer(routing_key="Tech Lab Key",exchange_name="Tech Lab Exchange")
+    producer = mqProducer(routing_key="Tech_Lab_Key",exchange_name="Tech_Lab_Exchange")
     producer.publishOrder("Success! Producer And Consumer Section Complete.")
 
 
